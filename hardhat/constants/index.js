465e40b297ad054efc23a2ec1b158f74fa94710c
Rafael Ortiz <raffaellortiz@gmail.com>
2022-05-22 22:44:15 -0500

    Update image URL

--- hardhat/constants/index.js
+++ hardhat/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x79b2bAF1E49a3821e3af743ad67E378A67536B0a";
 // URL to extract Metadata for a Dragon Dev NFT
-const METADATA_URL = "/api/";
+const METADATA_URL = "https://a-nft-collection.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
--- my-app/pages/api/[tokenId].js
+++ my-app/pages/api/[tokenId].js
@@ -4,7 +4,7 @@ export default function handler(req, res) {
   const tokenId = req.query.tokenId;
   const name = `Dragon Dev #${tokenId}`;
   const description = "Dragon Dev is a NFT collection fror web3 devs."
-  const image = `https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/${Number(tokenId) - 1}.svg`;
+  const image = `https://a-nft-collection.vercel.app/cryptodevs/${Number(tokenId) - 1}.svg`;
 
   return res.json({
     name: name,
